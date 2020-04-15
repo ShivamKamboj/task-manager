ad22483a1c027e272df4f0927e307647638ef4d9
Shivam Kamboj <shivam.kamboj@nagarro.com>
2020-04-15 13:30:28 +0530

    using sqlite for database connection

--- config/database.js
+++ config/database.js
@@ -1,13 +1,36 @@
 const sequelize = require('sequelize')
 
-module.exports = new sequelize('taskmanager', 'root', 'root', {
-    host: 'localhost',
-    dialect: 'mysql',
+const model = require('../models/modelHandler')
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-})
+// module.exports = new sequelize('taskmanager', 'root', 'root', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// })
+
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: __dirname + '/taskmanager.db'
+});
+
+const task = model.task.createTaskModel(db, Sequelize);
+const note = model.note.createNoteModel(db, Sequelize);
+
+task.hasMany(note, {
+    onDelete: 'CASCADE',
+    hooks: true,
+});
+
+note.belongsTo(task);
+
+module.exports = {
+    queryInterface: db,
+    task: task,
+    note: note
+};
