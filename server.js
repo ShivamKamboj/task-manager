2ff5586e08b5d16bd9707f9e17c3f1a94bb6a13c
Shivam Kamboj <shivam.kamboj@nagarro.com>
2020-04-15 14:03:31 +0530

    as new sever to start application

--- server.js
+++ server.js
@@ -1,40 +1,63 @@
-// const express=require("express")
-// const bodyParser=require("body-parser")
-// const mysqlConnection=require("./connection")
+const express = require("express")
+    //const exphbs = require("express-handlebars")
+const bodyParser = require("body-parser")
+const path = require('path')
+    //database
+const db = require("./config/database")
 
-// var app=express()
-// //this configures the node js application
+var app = express()
+app.use(express.json())
+
+
+//test db
+db.queryInterface.sync().then((res) => {
+    console.log("Connection..")
+}).catch((err) => {
+    console.log("Error");
+});
+// db.authenticate()
+//     .then(() => console.log("Connection worked"))
+//     .catch((err) => console.error('Error :', err))
+
+
+//     //this configures the node js application
 // app.use(bodyParser.json())
-// //to use clientside
-// app.use('/',express.static(__dirname+'/clientside'))
+//     //to use clientside
+// app.use('/', express.static(__dirname + '/clientside'))
 
 // //try for get post requests
-// let todos=[
-//     {
-//         titel:'no1',
-//         description:'its first',
-//         due_date:'2020-04-06',
-//         priority:'medium',
-//         status:'incomplete'
+// let todos = [{
+//         titel: 'no1',
+//         description: 'its first',
+//         due_date: '2020-04-06',
+//         priority: 'medium',
+//         status: 'incomplete'
 //     },
 //     {
-//         titel:'no2',
-//         description:'its second',
-//         due_date:'2020-04-16',
-//         priority:'low',
-//         status:'complete'
+//         titel: 'no2',
+//         description: 'its second',
+//         due_date: '2020-04-16',
+//         priority: 'low',
+//         status: 'complete'
 //     },
 //     {
-//         titel:'no3',
-//         description:'its third',
-//         due_date:'2020-04-26',
-//         priority:'high',
-//         status:'incomplete'
+//         titel: 'no3',
+//         description: 'its third',
+//         due_date: '2020-04-26',
+//         priority: 'high',
+//         status: 'incomplete'
 //     }
 // ]
 
-// app.get('/todos',(req,res)=>{
-//     res.send(todos)
-// })
-// //to start the application
-// app.listen(6555)
+//creating route
+app.get('/', (req, res) => {
+    res.send('INDEX')
+        //res.send(todos)
+})
+
+//taskmanager routes
+app.use('/tasks', require('./routes/tasks'))
+
+const PORT = process.env.PORT || 6556;
+//to start the application
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
