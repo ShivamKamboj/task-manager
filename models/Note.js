f9e72a1bc5a3f20aba2f3b9f6c21ee8bdbc69a1f
Shivam Kamboj <shivam.kamboj@nagarro.com>
2020-04-15 13:16:07 +0530

    creating model for note table

---
+++ models/Note.js
@@ -0,0 +1,21 @@
+const createModelNote = function createModelNote(queryInterface, sequelize) {
+    const Note = queryInterface.define('note', {
+        // id for each task
+        id: {
+            type: sequelize.DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: sequelize.DataTypes.UUIDV4,
+            allowNull: true
+        },
+        // content for notes, should be string and not-null
+        content: {
+            type: sequelize.STRING,
+            allowNull: false
+        },
+    });
+    return Note;
+}
+
+module.exports = {
+    createNoteModel: createModelNote
+}
