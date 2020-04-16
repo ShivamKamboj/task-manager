cda3ed20e282105f2f076124eccd302e95d061db
Shivam Kamboj <shivam.kamboj@nagarro.com>
2020-04-17 01:10:43 +0530

    Fixed bugs.

--- serverside/config/database.js
+++ serverside/config/database.js
@@ -2,18 +2,6 @@ const sequelize = require('sequelize')
 
 const modelObject = require('../models/modelHandler')
 
-// module.exports = new sequelize('taskmanager', 'root', 'root', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// })
-
 const db = new sequelize({
     dialect: 'sqlite',
     storage: __dirname + '/taskmanager.db'
--- serverside/server.js
+++ serverside/server.js
@@ -1,6 +1,4 @@
 const express = require("express")
-    //const exphbs = require("express-handlebars")
-    //const bodyParser = require("body-parser")
 const path = require('path')
     //database
 const db = require("./config/database")
@@ -13,7 +11,7 @@ express.urlencoded({ extended: true })
 app.use(express.json())
 
 //connect to clientside
-app.use('/',  express.static(path.join(__dirname,  '../clientside')))
+app.use('/', express.static(path.join(__dirname, '../clientside')))
 
 //allow operations over database
 app.use((req, res, next) => {
@@ -27,57 +25,8 @@ app.use((req, res, next) => {
 app.use(routes.task)
 app.use(routes.note)
 
-//test db
-// db.queryInterface.sync().then((res) => {
-//     console.log("Connection..")
-// }).catch((err) => {
-//     console.log("Error")
-// })
-// db.authenticate()
-//     .then(() => console.log("Connection worked"))
-//     .catch((err) => console.error('Error :', err))
-
-//this configures the node js application
-// app.use(bodyParser.json())
-//     //to use clientside
-// app.use('/', express.static(__dirname + '/clientside'))
-
-// //try for get post requests
-// let todos = [{
-//         titel: 'no1',
-//         description: 'its first',
-//         due_date: '2020-04-06',
-//         priority: 'medium',
-//         status: 'incomplete'
-//     },
-//     {
-//         titel: 'no2',
-//         description: 'its second',
-//         due_date: '2020-04-16',
-//         priority: 'low',
-//         status: 'complete'
-//     },
-//     {
-//         titel: 'no3',
-//         description: 'its third',
-//         due_date: '2020-04-26',
-//         priority: 'high',
-//         status: 'incomplete'
-//     }
-// ]
-
-//creating route
-// app.get('/', (req, res) => {
-//     res.send('INDEX')
-//         //res.send(todos)
-// })
-
-//taskmanager routes
-//app.use('/tasks', require('./routers/tasks'))
 
 const PORT = process.env.PORT || 6556;
-//to start the application
-//app.listen(PORT, console.log(`Server started on port ${PORT}`))
 
 (async function() {
     try {
