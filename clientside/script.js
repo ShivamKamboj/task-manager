f945a6687f334e02111401a676992b87b98c7383
Shivam Kamboj <shivam.kamboj@nagarro.com>
2020-04-16 23:40:44 +0530

    removing bugs and unused attributes

--- clientside/script.js
+++ clientside/script.js
@@ -434,7 +434,7 @@ let sortingObject = {
 
 /* for mobile devices*/
 // Script to open and close sidebar when on tablets and phones
-function w3_open() {
+function my_open() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
 }
@@ -442,34 +442,4 @@ function w3_open() {
 function my_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
-}
-
-// Slideshow Apartment Images
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-
-function currentDiv(n) {
-    showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    if (n > x.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = x.length
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
-    }
 }
